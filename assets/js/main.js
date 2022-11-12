a4cdfdda6ea8ea58772315ec2cf28359ec264567
Edonabb <eb47854@ubt-uni.net>
2022-11-12 13:01:08 -0800

    styled

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,6 +78,8 @@ const sendEmail = (e) =>{
             contactMessage.textContent = ''
         }, 5000)
     
+    },(error) =>{
+        alert('OOPS! SOMETHING HAS FAILED...', error)
     }
     )}  
   
@@ -87,12 +89,46 @@ contactForm.addEventListener('submit', sendEmail)
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+   const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >=350 ? scrollUp.classList.add('show-scroll')
+    : scrollUp.classList.remove('show-scroll')
+   }
+   window.addEventListener('scroll',scrollUp)
 
 /*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+if(selectedTheme){
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+
+}
+
+themeButton.addEventListener('click', () =>{
+document.body.classList.toggle(darkTheme)
+themeButton.classList.toogle(iconTheme)
+
+localStorage.setItem('selected-theme', getCurrentTheme())
+localStorage.setItem('selected-icon', getCurrentIcon())
+
+})
 
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
