1fccb8124e8d7c8ef2b19a0163914ef3e8ed2376
Edonabb <eb47854@ubt-uni.net>
2022-11-12 04:49:32 -0800

    new change

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,7 +27,28 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*=============== SWIPER PROJECTS ===============*/
+let swiperProjects = new Swiper(".container ", {
+  spaceBetween: 24,
+  loop: true,
+  grabCursor: true,
 
+  pagination:{
+    el: ".swiper-pagination",
+    clickable:true,
+  },
+
+  breakpoints:{
+    576: {
+        slidesPerView: 2,
+    },
+    768: {
+        slidesPerView: 2,
+        spaceBetween: 48,
+    }
+
+}
+  
+  });
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
